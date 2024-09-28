42874fb0a1c68a4b4478febd5d03e09a879232f5
Guilherme220501 <guilherme8876sk@gmail.com>
2024-09-28 03:08:10 -0300

    first

---
+++ script.js
@@ -0,0 +1,2 @@
+function showAlert(message) {window.onload = function () {alert(message);};}
+function toggleText() {var text = document.getElementById("text"); if (text.style.display === "none" || text.style.display === "") { text.style.display = "block"; } else { text.style.display = "none";}  }
